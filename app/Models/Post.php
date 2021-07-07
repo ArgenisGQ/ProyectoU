7b19f03b4ebf996cb377a6d4f0121bd1b3dd2b2c
Argenis Gil <argenis.gq@gmail.com>
2021-07-06 21:08:15 -0400

    creando las relaciones de la DB

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    //Relacion de uno a muchos inversa
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    //Relacion de muchos a muchos
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    //Relacion uno a uno poliformica
+    public function image(){
+        return $this->newMorphOne(Image::class, 'imageable')
+    }
 }
