83946250ffba3b64346cbab4c75630c41f1f0111
Sala de Profesores 01 <dead.salap01@uny.edu.ve>
2021-07-08 15:41:49 -0400

    colocando faker en TagFActory

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -2,9 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use Illuminate\Support\Str;
 class TagFactory extends Factory
 {
     /**
@@ -21,8 +23,17 @@ class TagFactory extends Factory
      */
     public function definition()
     {
+        $name = $this->faker->unique()->sentence();
+
         return [
             //
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'extract' => $this->faker->text(250),
+            'body' => $this->faker->text(2000),
+            'status' => $this->faker->randomElement([1,2]),
+            'category_id' => Category::all()->random->id,
+            'user_id' => User::all()->random()->id
         ];
     }
 }
