a803da4db01ec5f33ac55e015e16a8a4bb87d632
ArgenisGQ <Argenis.GQ@gmail.com>
2021-08-17 09:32:31 -0400

    (Muestras de posts - Terminado)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,9 +9,13 @@ class PostController extends Controller
 {
     public function index(){
 
-        $posts = Post::where('status',2)->get();
+        $posts = Post::where('status',2)->latest()->paginate(8);
         return view('posts.index', compact('posts'));
 
+    }
+
+    public function show(Post $post){
+        return $post;
     }
 
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,32 @@
 <x-app-layout>
     <div class="container py-8">
-        <div class="grid grid-cols-3 gap-6">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($posts as $post)
-                {{-- <article> --}}
-                <article class="w-full h-80 bg-cover bg-center" style="background-image: url({{Storage::url($post->image->url)}})">
+                <article class="w-full h-80 bg-cover bg-center @if($loop->first) md:col-span-2 @endif"  style="background-image: url({{Storage::url($post->image->url)}})">
+                    <div class="w-full h-full px-8 flex flex-col justify-center">
+                        <div>
+                            @foreach ($post->tags as $tag)
+                                <a href="" class="inline-block px-3 h-6 bg-gray-600 text-white round-full">
+                                {{$tag->name}}
+                                </a>
+                            @endforeach
+                        </div>
+
+                        <h1 class="text-4xl text-white leading-8 font-bold">
+                            <a href="">
+                                {{$post->name}}
+                            </a>
+                        </h1>
+                    </div>
+                    {{--  http://proyectouny.uny.pc//storage/posts/75746ce0f74fb5282628fe78b253558d.png --}}
                     {{-- <img src="{{Storage::url($post->image->url)}}" alt=""> --}}
                     {{-- {{Storage::url($post->image->url)}} --}}
                     {{-- {{$post->image->url}} --}}
                 </article>
             @endforeach
+        </div>
+        <div class="mt-4">
+            {{$posts->links()}}
         </div>
     </div>
 </x-app-layout>
