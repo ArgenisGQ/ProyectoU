a074ca58fc347f3f523b4fdb2d13c6a30ca93174
Argenis Gil <argenis.gq@gmail.com>
2021-08-23 21:04:15 -0400

    AdminLTE - Crud Post - Parte I

--- app/Http/Livewire/Admin/PostsIndex.php
+++ app/Http/Livewire/Admin/PostsIndex.php
@@ -6,11 +6,31 @@ use Livewire\Component;
 
 use App\Models\Post;
 
+use Livewire\WithPagination;
+
 class PostsIndex extends Component
 {
+    //para usar paginacion en livewire
+    use WithPagination;
+
+    //para usar los estilos de bootstrap y no los de livewire
+    protected $paginationTheme = "bootstrap";
+
+    //para usar con el holder de busqueda
+    public $search;
+
+    //
+    public function updatingSearch(){
+        $this->resetPage();
+    }
+
+
     public function render()
     {
-        $posts = Post::where('user_id', auth()->user()->id)->paginate();
+        $posts = Post::where('user_id', auth()->user()->id)
+                ->where('name', 'LIKE','%'. $this->search .'%')  //filtro para BUSCADOR
+                ->latest('id') //para colocar el ultimo de primero
+                ->paginate();  //paginar el orden de todo
 
         return view('livewire.admin.posts-index', compact('posts'));
     }
--- resources/views/livewire/admin/posts-index.blade.php
+++ resources/views/livewire/admin/posts-index.blade.php
@@ -1,29 +1,46 @@
 <div class="card">
-    <div class="card-body">
-        <thead>
-            <th>ID</th>
-            <th>Name</th>
-            <th colspan="2"></th>
-        </thead>
 
-        <Tbody>
-            @foreach ($posts as $post)
-                <tr>
-                    <td>{{$post->id}}</td>
-                    <td>{{$post->name}}</td>
-                    <td with="10px">
-                        <a href="{{route('admin.posts.edit', $post)}}" class="btn btn-primary btn-sm">Editar</a>
-                    </td>
-                    <td with="10px">
-                        <form action="{{route('admin.posts.destroy', $post)}}" method="POST">
-                            @csrf
-                            @method('DELETE')
-
-                            <button class="btn btn-danger btn-sm" type="submit">Eliminar</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </Tbody>
+    <div class="card-header">
+        <input wire:model="search" class="form-control" placeholder="Ingrese el nombre del post">
     </div>
+
+    @if ($posts->count())
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th colspan="2"></th>
+                </thead>
+
+                <Tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td>{{$post->id}}</td>
+                            <td>{{$post->name}}</td>
+                            <td with="10px">
+                                <a href="{{route('admin.posts.edit', $post)}}" class="btn btn-primary btn-sm">Editar</a>
+                            </td>
+                            <td with="10px">
+                                <form action="{{route('admin.posts.destroy', $post)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <button class="btn btn-danger btn-sm" type="submit">Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </Tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            {{$posts->links()}}
+        </div>
+    @else
+        <div class="card-body">
+            <strong>No hay ningun regristro...</strong>
+        </div>
+    @endif
+
 </div>
