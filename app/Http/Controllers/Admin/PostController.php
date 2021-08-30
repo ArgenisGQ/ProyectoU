cb04677a3420dad7177a9dcde34c63d120ba4696
ArgenisGQ <Argenis.GQ@gmail.com>
2021-08-30 14:44:52 -0400

    AdminLTE - instalado POLICE para controlar edicion post y vista de etiquetas

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -82,6 +82,8 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
+        $this->authorize('author', $post);
+
         $categories = Category::pluck('name', 'id');
 
         $tags = Tag::all();
@@ -99,6 +101,8 @@ class PostController extends Controller
      */
     public function update(PostRequest $request, Post $post)
     {
+        $this->authorize('author', $post);
+
         $post->update($request->all());
 
         if ($request->file('file')) {
@@ -137,6 +141,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        $this->authorize('author', $post);
+
         $post->delete();
 
         return redirect()->route('admin.posts.index', $post)->with('info', 'El post se elimino con exito');
