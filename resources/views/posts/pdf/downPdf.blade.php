addb868bb06e7200dac0a336d65ad456930feccd
ArgenisGQ <Argenis.GQ@gmail.com>
2021-09-16 15:05:08 -0400

    GuiaU01-PDf-numero de paginas - Base

--- resources/views/posts/pdf/downPdf.blade.php
+++ resources/views/posts/pdf/downPdf.blade.php
@@ -15,9 +15,16 @@
             margin-left: 0.5cm;
             margin-right: 0;
         }
+        .page-break {
+            page-break-after: always;
+        }
     </style>
+
+
 </head>
 <body style="margin: 50px 40px">
+
+    
 
     <div class="container">
         <h2 class="text-center table-dark">Redaccion de Actividades de Aprendizaje</h2>
@@ -126,4 +133,13 @@
     </div>
 
 </body>
+
+<script type="text/php">
+    if ( isset($pdf) ) {
+        $pdf->page_script('
+            $font = $fontMetrics->get_font("Arial, Helvetica, sans-serif", "normal");
+            $pdf->text(270, 730, "Pagina $PAGE_NUM de $PAGE_COUNT", $font, 10);
+        ');
+    }
+</script>
 </html>
