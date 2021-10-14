6d9eb27526c5a384dc26676bac19305b285b655e
ArgenisGQ <Argenis.GQ@gmail.com>
2021-10-14 09:21:15 -0400

    Acti01-Creado listado de usuarios

--- app/Http/Livewire/Admin/Usersindex.php
+++ app/Http/Livewire/Admin/Usersindex.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Usersindex extends Component
 {
+    use WithPagination;
+
+    public $search;
+
+    protected $paginationTheme = "bootstrap";
+
+    public function updatingSearch(){
+        $this->resetPage();
+    }
+
     public function render()
     {
-        return view('livewire.admin.usersindex');
+        $users = User::where('name', 'LIKE', '%' . $this->search. '%')
+                        ->orWhere('email', 'LIKE', '%' . $this->search. '%')
+                        ->paginate();
+
+        return view('livewire.admin.usersindex', compact('users'));
     }
 }
