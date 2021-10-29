5609ad7009b87d4416fdf4fb3714a53836c33547
ArgenisGQ <Argenis.GQ@gmail.com>
2021-10-29 09:02:02 -0400

    Cata01-Activado mostrar posts aleatoriamente

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,25 @@ class PostController extends Controller
 {
     public function index(){
 
-        $posts = Post::where('status',2)->latest()->paginate(4);
+        /* Aleatorio */
+        $posts = Post::where('status',2)
+                ->inRandomOrder()
+                ->limit(4)
+                ->paginate(4)
+                /* ->get() */
+                ;
+
+        /* $posts = Post::whereHas('status', function ($query) {
+                $query->where('status', '=', '2');})
+                ->orderByRaw("RAND()")
+                ->limit(4);
+ */
+        /* dd($posts); */
+        /* return $posts; */
+
+        /* En orden decendente */
+        /* $posts = Post::where('status',2)->latest()->paginate(4); */
+
         return view('posts.index', compact('posts'));
 
     }
@@ -39,6 +57,11 @@ class PostController extends Controller
                             ->get();
 
         return view('posts.show', compact('post', 'similares', 'categoria'));
+
+
+
+
+
 
         /* return $category_post; */
         /* return $post; */
