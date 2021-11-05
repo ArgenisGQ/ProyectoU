2da77b1b561ffcd535d2c870051e201647fe1a9c
Argenis Gil <argenis.gq@gmail.com>
2021-11-04 21:36:46 -0400

    Cata02-corrigiendo

---
+++ resources/views/livewire/admin/usersindex.blade.php
@@ -0,0 +1,47 @@
+<div>
+    <div class="card-header">
+        <input wire:model="search" class="form-control" placeholder="Ingrese el nombre del usuario">
+    </div>
+
+    @if ($posts->count())
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <th>ID</th>
+                    <th>Nombre de la Actividad</th>
+                    <th colspan="2"></th>
+                </thead>
+
+                <Tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td>{{$post->id}}</td>
+                            {{-- <td>{{$post->name}}</td> --}}
+                            <td><a href="{{route('posts.show', $post)}}">
+                                {{$post->name}}
+                            </a></td>
+                            <td with="10px">
+                                <a href="{{route('admin.posts.edit', $post)}}" class="btn btn-primary btn-sm">Editar</a>
+                            </td>
+                            <td with="10px">
+                                <form action="{{route('admin.posts.destroy', $post)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <button class="btn btn-danger btn-sm" type="submit">Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </Tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            {{$posts->links()}}
+        </div>
+    @else
+        <div class="card-body">
+            <strong>No hay ningun registro...</strong>
+        </div>
+    @endif
+</div>
