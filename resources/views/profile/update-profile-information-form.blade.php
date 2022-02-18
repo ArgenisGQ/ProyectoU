4b89e296a7be6f3b001777a85138c43a119c7bde
Argenis Gil <argenis.gq@gmail.com>
2022-02-18 15:36:08 -0400

    Cata03 - colocar roles en vista de profile

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -79,6 +79,65 @@
             <x-jet-input id="username" type="text" class="mt-1 block w-full" wire:model.defer="state.username" autocomplete="username" />
             <x-jet-input-error for="username" class="mt-2" />
         </div>
+
+
+        <!-- Roles -->
+        {{-- @foreach ($roles as $role)
+            <div class="col-span-6 sm:col-span-4">
+                <x-jet-checkbox name={{$role->name}} id={{$role->id}}/>
+                <div class="ml-2">
+
+                </div>
+            </div>
+        @endforeach --}}
+
+        <div class="col-span-6 sm:col-span-4">
+            <div class="field">
+                <label class="label is-small">Sports:</label>
+                <div class="control">
+                  <label class="checkbox">
+                    <input type="checkbox" name="football" value="1" {{ Auth::user()->football === '1' ? 'checked' :''}} wire:model.defer="state.football">
+                        Football
+                    </label>
+                    <label class="checkbox">
+                    <input type="checkbox" name="rugby" value="1"  {{ Auth::user()->rugby === '1' ? 'checked' :''}}>
+                        Rugby
+                    </label>
+                    <label class="checkbox">
+                    <input type="checkbox" name="tennis" value="1"  {{ Auth::user()->tennis === '1' ? 'checked' :''}}>
+                        Radio
+                    </label>
+                </div>
+            </div>
+        </div>
+
+        
+
+        @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+                <div class="col-span-6 sm:col-span-4">
+                    <x-jet-label for="terms">
+                        <div class="flex items-center">
+                            <x-jet-checkbox name="terms" id="terms"/>
+                            <div class="ml-2">
+                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
+                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
+                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
+                                ]) !!}
+                            </div>
+                        </div>
+                    </x-jet-label>
+                </div>
+        @endif
+
+        {{-- <div>
+            <label>
+                {!! Form::checkbox('roles[]', $role->id, null, ['class' => 'mr-1']) !!}
+
+            </label>
+        </div>
+ --}}
+
+    </label>
     </x-slot>
 
     <x-slot name="actions">
