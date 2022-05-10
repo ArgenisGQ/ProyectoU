0ced378f10b38d8f109f4932faf9e494b74ea55f
Argenis Gil <argenis.gq@gmail.com>
2022-05-10 16:14:46 -0400

    Acti02 - configurando el orden para DASHBOARD

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -210,56 +210,10 @@
           <div class="row">
             <!-- Left col -->
             <div class="col-md-8">
-              <!-- MAP & BOX PANE -->
-              <div class="card">
-                <div class="card-header">
-                  <h3 class="card-title">US-Visitors Report</h3>
 
-                  <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                      <i class="fas fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove">
-                      <i class="fas fa-times"></i>
-                    </button>
-                  </div>
-                </div>
-                <!-- /.card-header -->
-                <div class="card-body p-0">
-                  <div class="d-md-flex">
-                    <div class="p-1 flex-fill" style="overflow: hidden">
-                      <!-- Map will be created here -->
-                      <div id="world-map-markers" style="height: 325px; overflow: hidden">
-                        <div class="map"></div>
-                      </div>
-                    </div>
-                    <div class="card-pane-right bg-success pt-2 pb-2 pl-4 pr-4">
-                      <div class="description-block mb-4">
-                        <div class="sparkbar pad" data-color="#fff">90,70,90,70,75,80,70</div>
-                        <h5 class="description-header">8390</h5>
-                        <span class="description-text">Visits</span>
-                      </div>
-                      <!-- /.description-block -->
-                      <div class="description-block mb-4">
-                        <div class="sparkbar pad" data-color="#fff">90,50,90,70,61,83,63</div>
-                        <h5 class="description-header">30%</h5>
-                        <span class="description-text">Referrals</span>
-                      </div>
-                      <!-- /.description-block -->
-                      <div class="description-block">
-                        <div class="sparkbar pad" data-color="#fff">90,50,90,70,61,83,63</div>
-                        <h5 class="description-header">70%</h5>
-                        <span class="description-text">Organic</span>
-                      </div>
-                      <!-- /.description-block -->
-                    </div><!-- /.card-pane-right -->
-                  </div><!-- /.d-md-flex -->
-                </div>
-                <!-- /.card-body -->
-              </div>
               <!-- /.card -->
               <div class="row">
-                <div class="col-md-6">
+                {{-- <div class="col-md-6">
                   <!-- DIRECT CHAT -->
                   <div class="card direct-chat direct-chat-warning">
                     <div class="card-header">
@@ -461,7 +415,7 @@
                     <!-- /.card-footer-->
                   </div>
                   <!--/.direct-chat -->
-                </div>
+                </div> --}}
                 <!-- /.col -->
 
                 <div class="col-md-6">
@@ -632,6 +586,8 @@
                   <a href="javascript:void(0)" class="btn btn-sm btn-secondary float-right">View All Orders</a>
                 </div>
                 <!-- /.card-footer -->
+
+                
               </div>
               <!-- /.card -->
             </div>
