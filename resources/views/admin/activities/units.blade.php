b91b2c2c8528b428e1cd6c763805fc016b278e4a
Argenis Gil <argenis.gq@gmail.com>
2022-06-01 15:58:23 -0400

    Acti02 -

--- resources/views/admin/activities/units.blade.php
+++ resources/views/admin/activities/units.blade.php
@@ -148,6 +148,40 @@
 
                     </tbody>
                 </table>
+
+                <div class="form-group">
+                    <p class="font-weight-bold">Materias</p>
+
+
+                    @foreach ( $courses as $curso )
+
+                        @php
+                            $cursox = App\Models\User_course::where('code',$curso->code)->get();
+                        @endphp
+
+                        <dt>{{ $curso->code.' '.$curso->course }} </dt>
+
+                                @foreach ($cursox as $cursoy )
+                                    <dl>
+                                        <dd>
+                                            <div>
+                                                <label>
+                                                    {!! Form::checkbox('courses[]', $cursoy->id, null, ['class' => 'mr-1']) !!}
+                                                    {{ $cursoy->section }}
+                                                </label>
+                                            </div>
+                                        </dd>
+                                    </dl>
+                                @endforeach
+
+                    @endforeach
+
+
+                    @error('courses')
+                        <br>
+                        <span class="text-danger">{{$message}}</span>
+                    @enderror
+                </div>
             </div>
         </div>
 
