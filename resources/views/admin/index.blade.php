a2f101f579c8877305cbfb1a073d1a6a64e9d334
Argenis Gil <argenis.gq@gmail.com>
2022-09-12 15:26:27 -0400

    Acti02 - agregada funcionalidad para dashboard, link que dirige la actividad en borrador a editar y en terminado a pdf

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -338,8 +338,14 @@
                             @endphp
                             <tr>
                             {{--  <td><a href={{route('activities.show', $activity)}}>{{$activity->id}}</a></td> --}}
-                                <td><a href={{route('activities.pdf.down', $activity)}}>{{$activity->id}}</a></td>
-                                <td>{{$activity->name}}</td>
+
+                                <td>{{$activity->id}}</a></td>
+                                @if ($activity->status == 1)
+                                    <td><a href={{route('admin.activities.edit', $activity)}}>{{$activity->name}}</a></td>
+                                @else
+                                    <td><a href={{route('activities.pdf.down', $activity)}}>{{$activity->name}}</a></td>
+                                @endif
+                                {{-- <td>{{$activity->name}}</td> --}}
                                 @if ($cursoData != 'no')
                                     <td>{{$cursoData->course}}</td>
                                 @else
@@ -401,11 +407,17 @@
                   <a href="{{route('admin.activities.index')}}" class="btn btn-sm btn-secondary float-right">Ver todas las Actividades</a>
                 </div>
                 <!-- /.card-footer -->
-
-
               </div>
               <!-- /.card -->
             </div>
+
+
+
+
+
+
+
+
             <!-- /.col -->
 
             <div class="col-md-4">
@@ -520,6 +532,110 @@
               <!-- /.card -->
             </div>
             <!-- /.col -->
+          </div>
+
+          {{-- --------- --}}
+
+          <div class = "row">
+
+            <div class="col-md-8">
+
+                <!-- /.card -->
+                <div class="row">
+
+                  <!-- /.col -->
+
+
+                  <!-- /.col -->
+                </div>
+
+                {{-- -- Latest Members -- --}}
+                <!-- /.row -->
+
+                <!-- TABLE: LATEST ORDERS -->
+                <div class="card">
+                  <div class="card-header border-transparent">
+                    <h3 class="card-title">Actividades Sumativas con ponderacion Cero</h3>
+
+                    <div class="card-tools">
+                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                      </button>
+                      <button type="button" class="btn btn-tool" data-card-widget="remove">
+                        <i class="fas fa-times"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <!-- /.card-header -->
+                  <div class="card-body p-0">
+                    <div class="table-responsive">
+                      <table class="table m-0">
+                        <thead>
+                        <tr>
+                          <th>ID Actividad</th>
+                          <th>Actividad</th>
+                          <th>Asignatura</th>
+                          <th>Unidad</th>
+                          <th>Ponderacion</th>
+                          <th>Status</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+
+
+                          @foreach ($activities as $activity )
+                              @php
+                                  $cursoId = App\Models\Activity_course::where('id_activity',$activity->id)->first();
+                                  /* dd($cursoId); */
+                                  if ($cursoId) {
+                                      $cursoData = App\Models\User_course::where('id', $cursoId->id_course)->first();
+                                  }   else {
+                                      $cursoData = 'no';
+                                  };
+
+                                  /* $cursoData = App\Models\User_course::where('id', $cursoId->id_course)->first(); */
+                                  /* dd($cursoData->course); */
+                              @endphp
+                              <tr>
+                              {{--  <td><a href={{route('activities.show', $activity)}}>{{$activity->id}}</a></td> --}}
+                              @if ($activity->status == 1 && $activity->evaluation == 0 && $activity->activity_type == 3)
+                                  <td>{{$activity->id}}</a></td>
+                                  <td><a href={{route('admin.activities.edit', $activity)}}>{{$activity->name}}</td>
+                                  @if ($cursoData != 'no')
+                                      <td>{{$cursoData->course}}</td>
+                                  @else
+                                      <td>--</td>
+                                  @endif
+                                  <td>{{$activity->unit}}</td>
+                                  <td>{{$activity->evaluation}}</td>
+                                  @if ($activity->status == 2)
+                                      <td><span class="badge badge-success">Terminada</span></td>
+                                  @else
+                                      <td><span class="badge badge-warning">Borrador</span></td>
+                                  @endif
+                              @endif
+                              {{-- @else
+                                  <td>-no hay cursos sin terminar--</td>
+                              @endif --}}
+
+                              </tr>
+                          @endforeach
+
+                      </table>
+                    </div>
+                    <!-- /.table-responsive -->
+                  </div>
+                  <!-- /.card-body -->
+                  <div class="card-footer clearfix">
+                    <a href="{{route('admin.activities.create')}}" class="btn btn-sm btn-info float-left">Crear nueva Actividad</a>
+                    <a href="{{route('admin.activities.index')}}" class="btn btn-sm btn-secondary float-right">Ver todas las Actividades</a>
+                  </div>
+                  <!-- /.card-footer -->
+                </div>
+                <!-- /.card -->
+              </div>
+
           </div>
 @stop
 
